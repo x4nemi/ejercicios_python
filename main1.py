87434e32170d11457871f8e00f49261494a312c6
Ximena <ximena.garcia7482@alumnos.udg.mx>
2021-03-12 22:35:41 -0600

    6 al 9

--- main1.py
+++ main1.py
@@ -23,6 +23,36 @@ from str import Str
 # print(t)
 
 #5
-p = Str()
-p.getStr()
-p.printStr()
+# p = Str()
+# p.getStr()
+# p.printStr()
+
+#6
+# valores_D = input("D: ")
+# lista_D = valores_D.split(",")
+
+# for i in lista_D:
+#     q = (2 * 50 * int(i) / 30) ** 0.5
+#     print(round(q), end=", ")
+
+# 7
+# x = int(input("x: "))
+# y = int(input("y: "))
+
+# m = [[0 for col in range(y)] for row in range(x)]
+
+# for i in range(x):
+#     for j in range(y):
+#         m[i][j] = i * j
+
+# print(m)
+
+# # 8
+# palabras = input("Dame: ")
+# lista = palabras.split(",")
+# lista.sort()
+# print(lista)
+
+# 9
+cadena = input("Cadena: ")
+print(cadena.upper())
